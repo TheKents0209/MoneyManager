522bdfb2732d676343e8a90fbd7d687c1d7a532d
Kenert <kenert2001@gmail.com>
2022-02-09 00:24:47 +0200

    Fixed model classes

--- app/src/main/java/com/example/moneymanager/data/model/Expense.kt
+++ app/src/main/java/com/example/moneymanager/data/model/Expense.kt
@@ -1,15 +1,20 @@
 package com.example.moneymanager.data.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
-import java.sql.Timestamp
 
-@Entity
+@Entity(foreignKeys = [ForeignKey(
+    entity = Account::class,
+    onDelete = CASCADE,
+    parentColumns = ["id"],
+    childColumns = ["accountId"])])
 data class Expense(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
-    val date: Timestamp,
-    val account: Account,
+    val date: Long,
+    val accountId: Long,
     val amount: Float,
     val description: String,
     val imagePath: String
--- app/src/main/java/com/example/moneymanager/data/model/Income.kt
+++ app/src/main/java/com/example/moneymanager/data/model/Income.kt
@@ -1,15 +1,19 @@
 package com.example.moneymanager.data.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-import java.sql.Timestamp
 
-@Entity
+@Entity(foreignKeys = [ForeignKey(
+    entity = Account::class,
+    onDelete = ForeignKey.CASCADE,
+    parentColumns = ["id"],
+    childColumns = ["accountId"])])
 data class Income(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
-    val date: Timestamp,
-    val account: Account,
+    val date: Long,
+    val accountId: Long,
     val amount: Float,
     val description: String,
     val imagePath: String
