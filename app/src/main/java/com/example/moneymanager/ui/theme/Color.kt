6d1923b679dd3224d13dfbd14d7b0a9c24bac34d
Kenert <kenert2001@gmail.com>
2022-02-08 16:29:19 +0200

    Color palettes

--- app/src/main/java/com/example/moneymanager/ui/theme/Color.kt
+++ app/src/main/java/com/example/moneymanager/ui/theme/Color.kt
@@ -6,3 +6,15 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+//Color palette
+val RichBlack = Color(0xFF071013)
+val Gray = Color(0xFF777877)
+val Snow = Color(0xFFFFF3F0)
+val Vermilion = Color(0xFFC54630)
+val BlueCrayola = Color(0xFF388ABC)
+
+//Extra
+val Flame = Color(0xFFEB5E28)
+val Gold = Color(0xFFE19214)
+
