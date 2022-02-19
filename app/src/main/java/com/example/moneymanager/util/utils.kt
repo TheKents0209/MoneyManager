690a37bdbf2e79338c19663175b4b0953539050b
Kenert <kenert2001@gmail.com>
2022-02-20 00:00:27 +0200

    Removing code warnings and code cleanup

--- app/src/main/java/com/example/moneymanager/util/utils.kt
+++ app/src/main/java/com/example/moneymanager/util/utils.kt
@@ -1,13 +1,12 @@
 package com.example.moneymanager.util
 
 import android.util.Log
-import java.lang.StringBuilder
 import java.time.LocalDate
 import java.time.format.TextStyle
 import java.util.*
 
 fun formatMonthDoubleDigits(monthNum: String): String {
-    if(monthNum.toInt() < 10) {
+    if (monthNum.toInt() < 10) {
         return "0${monthNum}"
     } else {
         return monthNum
@@ -19,18 +18,20 @@ fun formatStringToDate(dateString: String): LocalDate {
 }
 
 fun formatLocalDateToString(date: LocalDate): String {
-    return "${date.dayOfMonth}.${date.monthValue}.${date.year} (${date.dayOfWeek.getDisplayName(
-        TextStyle.SHORT, Locale.getDefault())})"
+    return "${date.dayOfMonth}.${date.monthValue}.${date.year} (${
+        date.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+    })"
 }
 
+//Sample taken from https://stackoverflow.com/questions/70058829/limit-number-of-digits-before-and-after-decimals-in-text-field-in-jetpack-compos
 fun getValidatedNumber(text: String): String {
     // Start by filtering out unwanted characters like commas and multiple decimals
     val filteredChars = text.filterIndexed { index, c ->
         c in "0123456789" ||                      // Take all digits
-                (c == '.' && text.indexOf('.') == index)  // Take only the first decimal
+        (c == '.' && text.indexOf('.') == index)  // Take only the first decimal
     }
     // Now we need to remove extra digits from the input
-    return if(filteredChars.contains('.')) {
+    return if (filteredChars.contains('.')) {
         val beforeDecimal = filteredChars.substringBefore('.')
         val afterDecimal = filteredChars.substringAfter('.')
         beforeDecimal.take(6) + "." + afterDecimal.take(2)    // If decimal is present, take first 6 digits before decimal and first 2 digits after decimal
@@ -42,7 +43,7 @@ fun getValidatedNumber(text: String): String {
 fun currencyStringToInt(input: String?): Int {
     val pattern = Regex("[.,]")
     val newVal = input?.replace(pattern, "")
-    if(newVal?.length == 0) {
+    if (newVal?.length == 0) {
         return 0
     } else {
         return newVal!!.toInt()
@@ -51,18 +52,18 @@ fun currencyStringToInt(input: String?): Int {
 
 
 fun intToCurrencyString(input: Int?): String {
-    if(input?.length()!! >= 3) {
+    if (input?.length()!! >= 3) {
         val str = input.toString()
         val sb = StringBuilder(str)
-        val newVal = sb.insert(str.length-2, ".").toString()
+        val newVal = sb.insert(str.length - 2, ".").toString()
         Log.d("INT", newVal)
         return newVal
-    }else {
+    } else {
         return "0.00"
     }
 }
 
-fun Int.length() = when(this) {
+fun Int.length() = when (this) {
     0 -> 1
     else -> kotlin.math.log10(kotlin.math.abs(toDouble())).toInt() + 1
 }
