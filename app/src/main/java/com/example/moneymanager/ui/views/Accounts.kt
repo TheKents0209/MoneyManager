7e2a2c80201b453e19cdde57417edb36ed64d7db
DDiyar <ddogru10@gmail.com>
2022-03-10 20:46:27 +0200

    styling for Accounts.kt

--- app/src/main/java/com/example/moneymanager/ui/views/Accounts.kt
+++ app/src/main/java/com/example/moneymanager/ui/views/Accounts.kt
@@ -1,10 +1,10 @@
 package com.example.moneymanager.ui.views
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
@@ -13,7 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.moneymanager.R
 import com.example.moneymanager.data.database.DB
@@ -46,23 +48,26 @@ fun AccountsScreen(navController: NavController) {
                 )
             }
         }
-        LazyColumn() {
+        LazyColumn(modifier = Modifier
+            .padding(start = 10.dp)) {
             val groups = listDifferentGroups(list)
             groups.forEach { groupString ->
-                item() {
-                    Text(groupString, color = Color.Red)
-                    Column() {
+                item {
+                    Text(groupString, color = MaterialTheme.colors.onBackground, fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                    Column {
                         list?.forEach {
                             if(groupString == it.group) {
                                 Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-                                    Text(text = it.name + intToCurrencyString(aViewModel.getAccountAmount(it.id).observeAsState().value), modifier = Modifier.align(
-                                        Alignment.CenterVertically))
+                                    Text(text = it.name + "  " + intToCurrencyString(aViewModel.getAccountAmount(it.id).observeAsState().value), modifier = Modifier
+                                        .align(Alignment.CenterVertically)
+                                        .padding(vertical = 4.dp)
+                                    )
                                     if(isModifyEnabled) {
                                         IconButton(onClick = {
                                             aViewModel.deleteAccount(Account(it.id, it.group, it.name, it.amount, it.includeInTotals))
                                         }) {
                                             Icon(
-                                                painterResource(R.drawable.ic_twotone_remove_circle_outline_24), contentDescription = "Delete account"
+                                                painterResource(R.drawable.ic_twotone_remove_circle_outline_24), contentDescription = "Delete account", tint = Color.Red
                                             )
                                         }
                                     }
